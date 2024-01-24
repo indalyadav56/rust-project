518ca0f3dc2b35b07ac55528b76d2546267b1026
indalkumaryadav <indalkumar56@gmail.com>
2024-01-24 09:27:57 +0530

    wip api crud

--- api-crud/src/main.rs
+++ api-crud/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpServer, App, get, web, Responder};
+
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new().service(greet)
+    })
+    .bind(("127.0.0.1", 8005))?
+    .run()
+    .await
 }
